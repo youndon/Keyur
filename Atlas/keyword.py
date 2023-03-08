55e3a43206f81a01dfc204336825211957d27196
Yon <bourntivoli@gmail.com>
2023-03-08 15:44:33 +0100

    Share Keyur to remote data.

---
+++ Atlas/keyword.py
@@ -0,0 +1,8 @@
+# keyword that allows us to declare a variable global inside a function's body.
+global glo
+
+
+# keyword lets us assign to variables in the outer (but not global) scope:
+# nonlocal c
+
+atlas = "atlas module"
